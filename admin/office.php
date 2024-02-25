1911189d7bef48967131edd0dea5e87fbc7ff594
BigBoyz2314 <zazimran@gmail.com>
2024-02-26 02:00:16 +0500

    added search

--- admin/office.php
+++ admin/office.php
@@ -341,6 +341,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 	<script>
 
