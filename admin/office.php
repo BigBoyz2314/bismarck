42d8fed7868a7ecd6c5b109d1e6a04a6ed115656
BigBoyz2314 <zazimran@gmail.com>
2024-02-27 02:17:27 +0500

    all year added

--- admin/office.php
+++ admin/office.php
@@ -132,6 +132,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                         <h6>Year</h6>
                         <select name="year" id="year" class="form-select" required>
                             <option value="">Select</option>
+                            <option value="All">All</option>
                             <option value="2020">2020</option>
                             <option value="2021">2021</option>
                             <option value="2022">2022</option>
@@ -270,7 +271,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                                                 echo "<td></td>";
                                             }
 
-                                        } else {
+                                        } elseif (!isset($_GET['year']) || $_GET['year'] == "All") {
                                             $sql4 = "SELECT SUM(balance) as balance FROM production WHERE office_id = ".$row['id']."";
                                             $result4 = $conn->query($sql4);
                                             if (!$result4) {
