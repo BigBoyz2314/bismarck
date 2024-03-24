ba279edf4063f1831b99da13b10af0b7ac84e498
BigBoyz2314 <zazimran@gmail.com>
2024-03-24 21:42:40 +0500

    added translate to client

--- client/bank-reg.php
+++ client/bank-reg.php
@@ -21,67 +21,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-nav">
-        <div class="container-fluid w-75">
-            <div class="d-flex flex-column py-1">
-                <a class="navbar-brand p-0 fw-bold text-light fs-3" href="#">Bismarck Business Group, LLC</a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="change-password.php">Change Password</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="logout.php">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </nav>
-
-
+    <?php include("header.php"); ?>
     
-    <nav class="navbar navbar-expand-lg bg-light text-primary border-bottom border-primary">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0 fw-semibold">
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="index.php">Overview</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="bank-reg.php">Bank Registration</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="software-reg.php">Software Registration</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="production.php">Production</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="registration-codes.php">Registration Codes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="installation.php">Installation & Updates</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="la-biblia.php">La Biblia</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="software.php">Software Management</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-primary" href="inquiries.php">Inquiries</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
 	<div class="container-fluid">
         <div class="row mt-3">
             <div class="col-md-12 mb-5 d-flex flex-column align-items-center">
@@ -254,6 +195,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         }
     </script>
 
-	<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <?php include('translate.php'); ?>
 </body>
 </html>
---
+++ client/translate.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: "en", includedLanguages: "es", layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, "google_translate_element");
+}
+
+</script>';
+
+echo '<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
+
+?>
